91a5067a61ac9757c46ddc78ba5723e27afba29f
aipronob <aipronob@gmail.com>
2026-03-01 22:35:24 +0600

    button corrected

--- script/interview.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("interview1").addEventListener("click", function(){
-  
-  const clicking1 = getValueFromClick(interview1);
-  const clicking2 = getValueFromClick(interview2);
-
-  
-});
